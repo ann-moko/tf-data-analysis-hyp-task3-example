2426848617b5ab3a92c09e662a93cfd4b5195374
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-13 23:01:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from scipy.stats import t
 
 chat_id = 371784753 # Ваш chat ID, не меняйте название переменной
 
@@ -9,6 +10,5 @@ def solution(x: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.1
-    t_statistic, p_value = stats.ttest_1samp(a=x, popmean=300)
+    stat, p_value = stats.ttest_1samp(a=x, popmean=300)
     return (p_value < alpha)   # Ваш ответ, True или False
-
