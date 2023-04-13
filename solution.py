038b9cbb46dd1b98f14cff22e6a0ea04f98efc17
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-13 18:41:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from scipy.stats import t
 
 chat_id = 371784753 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: 
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x: np.array) -> bool:
     alpha = 0.1
-    t_statistic, p_value = stats.ttest_1samp(a=x, popmean=300)
-    return (p_value < alpha)   # Ваш ответ, True или False
+    threshold = 300
+    n = len(x) 
+    mean = np.mean(x)
+    std = np.std(x, ddof=1)
+    t_stat = (mean - threshold) / (std / np.sqrt(n))
+    t_crit = t.ppf(1 - alpha, n - 1)
+    return t_stat < t_crit
